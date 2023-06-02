afd1c367681b83b93cf15acb9e2498e923d954ec
DrFatihTekin <42251918+DrFatihTekin@users.noreply.github.com>
2023-06-02 16:00:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,15 @@
 let distance = 0
 keyboard.ServoRun(keyboard.aServos.S1, 0)
 basic.forever(function () {
-    if (distance < 3) {
-        keyboard.ServoRun(keyboard.aServos.S1, 70)
-        basic.pause(5000)
-        keyboard.ServoRun(keyboard.aServos.S1, 0)
-    }
-})
-basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P0,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
+    if (distance < 3) {
+        keyboard.ServoRun(keyboard.aServos.S1, 70)
+        basic.pause(5000)
+        keyboard.ServoRun(keyboard.aServos.S1, 0)
+    }
     basic.pause(1000)
 })
