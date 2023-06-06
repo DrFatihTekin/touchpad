d903f8207b0a0517974b108917c6b0ddbddefa89
DrFatihTekin <42251918+DrFatihTekin@users.noreply.github.com>
2023-06-06 20:43:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    show_distance = 0
+})
+input.onButtonPressed(Button.B, function () {
+    show_distance = 1
+})
 let distance = 0
+let show_distance = 0
 keyboard.ServoRun(keyboard.aServos.S1, 0)
+basic.showIcon(IconNames.Happy)
+show_distance = 0
 loops.everyInterval(500, function () {
     distance = sonar.ping(
     DigitalPin.P0,
@@ -7,10 +16,14 @@ loops.everyInterval(500, function () {
     PingUnit.Centimeters
     )
     if (distance < 10 && distance != 0) {
-        basic.showIcon(IconNames.Heart)
         keyboard.ServoRun(keyboard.aServos.S1, 40)
+        basic.showIcon(IconNames.Heart)
         basic.pause(5000)
         keyboard.ServoRun(keyboard.aServos.S1, 0)
     }
-    basic.showIcon(IconNames.Happy)
+    if (show_distance == 1) {
+        basic.showNumber(distance)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
